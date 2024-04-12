b7309fc22c02d4d4a950c2835ce2504d9b04d9ef
rolandoimmanuel <rolando.immanuel@gmail.com>
2024-04-12 21:32:28 +0700

    Final Done

--- todo_main/views.py
+++ todo_main/views.py
@@ -1,4 +1,12 @@
 from django.shortcuts import render
+from todo.models import Task
 
 def home(request):
-        return render(request, 'home.html')
+        tasks = Task.objects.filter(is_completed=False)
+        completed_tasks = Task.objects.filter(is_completed=True)
+        
+        context = {
+            'tasks' : tasks,
+            'completed_tasks' : completed_tasks,
+        }
+        return render(request, 'home.html', context)
